3c595617d29c70f381de2f1287ff8db062fa71bf
krists0 <48677778+krists0@users.noreply.github.com>
2021-06-14 21:59:49 +0300

    Update add_new_employee.js

--- server/validation/add_new_employee.js
+++ server/validation/add_new_employee.js
@@ -27,30 +27,14 @@ module.exports = function validateNewEmployeeInput(data) {
         errors.emp_last_name = 'Last Name field is required';
     }
 
-    // if (Validator.isEmpty(data.emp_email)) {
-    //     errors.emp_email = 'Email field is required';
-    // }
+   
     if(data.emp_email) {
         if (!Validator.isEmail(data.emp_email)) {
             errors.emp_email = 'Email is invalid';
         }
     }
 
-    // if (Validator.isEmpty(data.password)) {
-    //     errors.password = 'Password field is required';
-    // }
-
-    // if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    //     errors.password = 'Password must be at least 6 characters';
-    // }
-
-    // if (Validator.isEmpty(data.password2)) {
-    //     errors.password2 = 'Confirm Password field is required';
-    // } else {
-    //     if (!Validator.equals(data.password, data.password2)) {
-    //         errors.password2 = 'Passwords must match';
-    //     }
-    // }
+   
 
     return {
         errors,
